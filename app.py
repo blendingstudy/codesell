93d3de95aec5802df3ad34795090b5e71db7ef7f
yeonje <kyounje@naver.com>
2024-03-15 20:28:04 +0900

    index

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from views import auth_bp, product_bp, cart_bp, funding_bp
+from views.auth import auth_bp #, product_bp, cart_bp, funding_bp
 from models import db
+from config import Config
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://your_username:your_password@localhost/your_database_name'
+app.config.from_object(Config)
 db.init_app(app)
 
 app.register_blueprint(auth_bp)
-app.register_blueprint(product_bp)
-app.register_blueprint(cart_bp)
-app.register_blueprint(funding_bp)
+#app.register_blueprint(product_bp)
+#app.register_blueprint(cart_bp)
+#app.register_blueprint(funding_bp)
 
 @app.route('/')
 def index():
@@ -24,4 +24,5 @@ def index():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+        print('db')
+    app.run(debug=False)
---
+++ controllers/auth_controller.py
@@ -0,0 +1,14 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from models import User, db
+
+def authenticate_user(username, password):
+    user = User.query.filter_by(username=username, password=password).first()
+    return user
+
+def register_user(username, password):
+    user = User(username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
