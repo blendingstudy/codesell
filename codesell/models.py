a8895a53f583eb256b56c14f1a9e5c7975aa93a5
yeonje <kyounje@naver.com>
2024-03-19 01:45:57 +0900

    login register logout

--- codesell/models.py
+++ codesell/models.py
@@ -1,13 +1,26 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from codesell import db
+#from werkzeug.security import generate_password_hash, check_password_hash
 
-#db = SQLAlchemy()
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    #password_hash = db.Column(db.String(128), nullable=False)
+
+    """ @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute') """
+
+    """ @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password) """
+
+    def check_password(self, password):
+        #return check_password_hash(self.password_hash, password)
+        return self.password == password
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
