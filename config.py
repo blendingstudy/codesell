eb1fadb83ecd91322b31cb99c33a64a16db97a52
yeonje <kyounje@naver.com>
2024-06-15 18:06:04 +0900

    config

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://kyounje:9350@localhost/codesell2'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://kyounje:9350@localhost/codesell'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # 추가 설정 옵션들
